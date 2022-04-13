72f62d70d9d994402bf6b199d8b30623a266fbb7
JaydenHall <jaydenhallpt@hotmail.co.nz>
2022-04-13 20:12:11 +1200

    carousel added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from "@angular/common";
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
